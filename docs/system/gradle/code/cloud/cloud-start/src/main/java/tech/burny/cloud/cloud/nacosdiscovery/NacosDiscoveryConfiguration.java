c488e8700be85e07c1d57e9c0bf0d1dcf64f2785
chenyingxin <1402421202@qq.com>
2023-05-23 00:26:09 +0800

    clear cached

---
+++ docs/system/gradle/code/cloud/cloud-start/src/main/java/tech/burny/cloud/cloud/nacosdiscovery/NacosDiscoveryConfiguration.java
@@ -0,0 +1,13 @@
+
+package tech.burny.cloud.cloud.nacosdiscovery;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
+ */
+@EnableDiscoveryClient
+@Configuration
+public class NacosDiscoveryConfiguration {
+}
